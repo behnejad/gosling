e2a501beee2be0414013579bd70f5b25dd492b98
hooman <behnejad@comp.iust.ac.ir>
2015-11-22 22:02:23 +0330

    problem can save(); now

--- statics/js/addproblem.js
+++ statics/js/addproblem.js
@@ -4,19 +4,19 @@
 
 var radio = false, radioAnswers = 0, innerRadioAnswer = 0, ids = [], radioStat = {};
 for (var i = 65; i <= 90; ++i) {
-	ids.push(String.fromCharCode(i));
-	ids.push(String.fromCharCode(i + 32));
+    ids.push(String.fromCharCode(i));
+    ids.push(String.fromCharCode(i + 32));
 }
 function initradioAnswers() {
-	if (!radio) {
-		$('<ol class="radio-answers"></ol>').clone().hide().prependTo('div.radio-answers').slideDown('fast');
-		radio = true;
-	}
+    if (!radio) {
+        $('<ol class="radio-answers"></ol>').clone().hide().prependTo('div.radio-answers').slideDown('fast');
+        radio = true;
+    }
 }
 $(document).on('click', 'a.radio-answer', function () {
-	initradioAnswers();
-	var parent = ids[2 * radioAnswers], child = ids[2 * innerRadioAnswer + 1];
-	temp = `<li class="radio-answer" id="` + parent + `">
+    initradioAnswers();
+    var parent = ids[2 * radioAnswers], child = ids[2 * innerRadioAnswer + 1];
+    temp = `<li class="radio-answer" id="` + parent + `">
 				<input class="addproblem radio-title" id="` + parent + `" placeholder="متن سوال">
 				<ul class="radio-answer-` + parent + `">
 					<li class="radio-inner-answer" id="` + child + `">
@@ -26,45 +26,45 @@ $(document).on('click', 'a.radio-answer', function () {
 				<a class="label label-danger pak-radio-answer hide" id="` + parent + `">حذف سوال</a>
 				<hr>
 			</li>`;
-	$(temp).clone().hide().appendTo('ol.radio-answers').slideDown('fast');
+    $(temp).clone().hide().appendTo('ol.radio-answers').slideDown('fast');
     radioStat[parent] = {};
     radioStat[parent][child] = false;
-	++radioAnswers;
-	++innerRadioAnswer;
+    ++radioAnswers;
+    ++innerRadioAnswer;
 });
 $('a.radio-answer').trigger("click");
 $(document).on('click', 'a.pak-radio-answer', function () {
-	$('li.radio-answer#' + $(this).attr('id')).slideUp('fast', function () {
-		$(this).remove();
-	});
+    $('li.radio-answer#' + $(this).attr('id')).slideUp('fast', function () {
+        $(this).remove();
+    });
 });
 $(document).on('change', 'input.addproblem', function () {
-	if ($(this).val() && radioStat[$(this).attr('parent')][$(this).attr('line')] == false) {
-		$(`<li class="radio-inner-answer" id="` + ids[2 * innerRadioAnswer + 1] + `">
+    if ($(this).val() && radioStat[$(this).attr('parent')][$(this).attr('line')] == false) {
+        $(`<li class="radio-inner-answer" id="` + ids[2 * innerRadioAnswer + 1] + `">
 	        <input class="addproblem radio-answer" line="` + ids[2 * innerRadioAnswer + 1] +
-			`" parent="` + $(this).attr('parent') + `" type="text" placeholder="متن گزینه"></li>`)
-			.clone().hide().appendTo('ul.radio-answer-' + $(this).attr('parent')).slideDown('fast');
+            `" parent="` + $(this).attr('parent') + `" type="text" placeholder="متن گزینه"></li>`)
+            .clone().hide().appendTo('ul.radio-answer-' + $(this).attr('parent')).slideDown('fast');
         radioStat[$(this).attr('parent')][$(this).attr('line')] = true;
-		radioStat[$(this).attr('parent')][ids[2 * innerRadioAnswer + 1]] = false;
+        radioStat[$(this).attr('parent')][ids[2 * innerRadioAnswer + 1]] = false;
         ++innerRadioAnswer;
-	}
-    else if (!$(this).val() && radioStat[$(this).attr('parent')][$(this).attr('line')] == true){
+    }
+    else if (!$(this).val() && radioStat[$(this).attr('parent')][$(this).attr('line')] == true) {
         $('li.radio-inner-answer#' + $(this).attr('line')).slideUp('fast', function () {
-			$(this).remove();
-		});
+            $(this).remove();
+        });
     }
     else {
         radioStat[$(this).attr('parent')][$(this).attr('line')] = true;
     }
 });
 var short = false, shortAnswers = 0;
-function initShortAnswer(){
+function initShortAnswer() {
     if (!short) {
-		$('<ol class="short-answers"></ol>').clone().hide().prependTo('div.short-answers').slideDown('fast');
-		short = true;
-	}
+        $('<ol class="short-answers"></ol>').clone().hide().prependTo('div.short-answers').slideDown('fast');
+        short = true;
+    }
 }
-$('a.short-answer').click(function(){
+$('a.short-answer').click(function () {
     initShortAnswer();
     var parent = ids[2 * shortAnswers];
     temp = `<li class="short-answer" id="` + parent + `">
@@ -72,22 +72,22 @@ $('a.short-answer').click(function(){
 				<a class="label label-danger pak-short-answer" id="` + parent + `">حذف سوال</a>
 				<hr>
 			</li>`;
-	$(temp).clone().hide().appendTo('ol.short-answers').slideDown('fast');
+    $(temp).clone().hide().appendTo('ol.short-answers').slideDown('fast');
     ++shortAnswers;
 });
 $(document).on('click', 'a.pak-short-answer', function () {
-	$('li.short-answer#' + $(this).attr('id')).slideUp('fast', function () {
-		$(this).remove();
-	});
+    $('li.short-answer#' + $(this).attr('id')).slideUp('fast', function () {
+        $(this).remove();
+    });
 });
 var long = false, longAnswers = 0;
-function initLongAnswer(){
+function initLongAnswer() {
     if (!long) {
-		$('<ol class="long-answers"></ol>').clone().hide().prependTo('div.long-answers').slideDown('fast');
-		long = true;
-	}
+        $('<ol class="long-answers"></ol>').clone().hide().prependTo('div.long-answers').slideDown('fast');
+        long = true;
+    }
 }
-$('a.long-answer').click(function(){
+$('a.long-answer').click(function () {
     initLongAnswer();
     var parent = ids[2 * longAnswers];
     temp = `<li class="long-answer" id="` + parent + `">
@@ -95,26 +95,39 @@ $('a.long-answer').click(function(){
 				<a class="label label-danger pak-long-answer" id="` + parent + `">حذف سوال</a>
 				<hr>
 			</li>`;
-	$(temp).clone().hide().appendTo('ol.long-answers').slideDown('fast');
+    $(temp).clone().hide().appendTo('ol.long-answers').slideDown('fast');
     ++longAnswers;
 });
 $(document).on('click', 'a.pak-long-answer', function () {
-	$('li.long-answer#' + $(this).attr('id')).slideUp('fast', function () {
-		$(this).remove();
-	});
+    $('li.long-answer#' + $(this).attr('id')).slideUp('fast', function () {
+        $(this).remove();
+    });
 });
-$('#clean').click(function(){
-	CKEDITOR.instances['problem'].setData('');
-	$('div.radio-answers').slideUp('fast', function (){
-		$(this).html('');
-
-		$(this).slideDown('fast');
-	});
-$('a.radio-answer').trigger("click");
+$('#clean').click(function () {
+    CKEDITOR.instances['problem'].setData('');
+    $('div.radio-answers').slideUp('fast', function () {
+        $(this).html('');
+        $(this).slideDown('fast');
+    });
+    $('a.radio-answer').trigger("click");
+});
+$('button#submit').click(function () {
+    var ans = '';
+    $('input.radio-answer').each(function (){
+        if ($(this).val() != '')
+            ans += $(this).val() + '|';
+    });
+    $.ajax({
+        type: "post",
+        url: "",
+        data: {final: true, index: state, text: CKEDITOR.instances['problem'].getData(),
+                question: $('input.radio-title').val(), answers: ans, csrftoken: $.cookie("csrftoken"),
+                csrfmiddlewaretoken: $("input[name='csrfmiddlewaretoken']").val()}
+    });
 });
 $(document).ready(function () {
-	window.CKEDITOR_BASEPATH = '/statics/ckeditor/ckeditor/';
-	$.getScript('/statics/ckeditor/ckeditor/ckeditor.js', function () {
-		CKEDITOR.replace('problem');
-	});
+    window.CKEDITOR_BASEPATH = '/statics/ckeditor/ckeditor/';
+    $.getScript('/statics/ckeditor/ckeditor/ckeditor.js', function () {
+        CKEDITOR.replace('problem');
+    });
 });
